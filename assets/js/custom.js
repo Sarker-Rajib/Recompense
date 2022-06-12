b4ca7faa268d6ac683e24c04d3b1783927fe0fc5
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-12 21:28:08 +0600

    12/6..9.28

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,8 @@
-// identification item chagne 
-function identificationFilename(inputItems, areaToShow){
-    const inpputName = document.querySelectorAll(inputItems);
-    const targetArea = document.querySelector(areaToShow);
-    [...inpputName].forEach((input) => {
-        input.addEventListener('change', function(e){
-            targetArea.value = e.target.value;
-        })
-    })
-}
+let copyText = document.getElementById("link");
+let copyButton = document.getElementById("copyBtn")
 
-// call the function identification;
-identificationFilename('input[name=nominee-identification-1]', '#nominee-identification-item-1');
-identificationFilename('input[name=nominee-identification-2]', '#nominee-identification-item-2');
-identificationFilename('input[name=nominee-guardian-identification-1]', '#nominee-guardian-identification-item-1');
-identificationFilename('input[name=nominee-guardian-identification-2]', '#nominee-guardian-identification-item-2');
-// call function ends;
+copyButton.addEventListener('click', function(e) {
 
-// pop-up
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the text: " + copyText.value);
+})
